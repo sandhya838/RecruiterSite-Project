aa55b05bfadea9cff6966e8f6a8e6b8b30d1fc6e
vedprakash arya <pvedprakasharya@gmail.com>
2021-11-12 12:38:52 +0530

    dashboard restructured

--- job portal/src/app/Components/admin/admin.module.ts
+++ job portal/src/app/Components/admin/admin.module.ts
@@ -5,9 +5,43 @@ import { AdminRoutingModule } from "./admin-routing.module";
 import { AdminComponent } from "./admin.component";
 import { DashboardComponent } from "../dashboard/dashboard.component";
 import { SharedModule } from "../shared/shared.module";
+import { JobProfileCardComponent } from "src/app/job-profile-card/job-profile-card.component";
+import { CertificationComponent } from "../certification/certification.component";
+import { CompanyDetailsComponent } from "../company-details/company-details.component";
+import { EduDetailsComponent } from "../edu-details/edu-details.component";
+import { ExperianceComponent } from "../experiance/experiance.component";
+import { PrimarySkillComponent } from "../primary-skill/primary-skill.component";
+import { RollprofileComponent } from "../rollprofile/rollprofile.component";
+import { SkillProfileComponent } from "../skill-profile/skill-profile.component";
+import { WorkExperianceComponent } from "../work-experiance/work-experiance.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgMultiSelectDropDownModule } from "ng-multiselect-dropdown";
+import { HttpClientModule } from "@angular/common/http";
+import { AngularMultiSelectModule } from "angular2-multiselect-dropdown";
 
 @NgModule({
-  declarations: [AdminComponent, DashboardComponent],
-  imports: [CommonModule, AdminRoutingModule,SharedModule],
+  declarations: [
+    AdminComponent,
+    DashboardComponent,
+    ExperianceComponent,
+    WorkExperianceComponent,
+    SkillProfileComponent,
+    EduDetailsComponent,
+    RollprofileComponent,
+    CertificationComponent,
+    PrimarySkillComponent,
+    CompanyDetailsComponent,
+    JobProfileCardComponent,
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgMultiSelectDropDownModule.forRoot(),
+    HttpClientModule,
+    AngularMultiSelectModule,
+  ],
 })
 export class AdminModule {}
--- job portal/src/app/services/jobs.service.ts
+++ job portal/src/app/services/jobs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { CONSTANTS } from "../constants";
 
 @Injectable({
@@ -8,20 +9,20 @@ import { CONSTANTS } from "../constants";
 export class JobsService {
   apiBaseUrl = CONSTANTS.BASEURL;
   constructor(private http: HttpClient) {}
-  // private _getHeaders() {
-  //   let header = new HttpHeaders({
-  //     "x-access-token": sessionStorage.getItem("token") as string,
-  //     "Content-Type": "application/json",
-  //   });
+  private _getHeaders() {
+    let header = new HttpHeaders({
+      "x-access-token": sessionStorage.getItem("token") as string,
+      "Content-Type": "application/json",
+    });
 
-  //   return header;
-  // }
-  // getRecommendedJobs(searchString: any): Observable<any> {
-  //   const header = this._getHeaders();
-  //   return this.http.post(
-  //     this.apiBaseUrl + CONSTANTS.RECOMMENDEDJOBS,
-  //     searchString,
-  //     { headers: header }
-  //   );
-  // }
+    return header;
+  }
+  getRecommendedJobs(searchString: any): Observable<any> {
+    const header = this._getHeaders();
+    return this.http.post(
+      this.apiBaseUrl + CONSTANTS.RECOMMENDEDJOBS,
+      searchString,
+      { headers: header }
+    );
+  }
 }
