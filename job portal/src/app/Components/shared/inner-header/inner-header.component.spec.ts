e45a31b17ffc52a5b9330bea86381583b4571f86
vedprakash arya <pvedprakasharya@gmail.com>
2021-11-12 10:22:20 +0530

    header and sidebar redesigned

---
+++ job portal/src/app/Components/shared/inner-header/inner-header.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InnerHeaderComponent } from './inner-header.component';
+
+describe('InnerHeaderComponent', () => {
+  let component: InnerHeaderComponent;
+  let fixture: ComponentFixture<InnerHeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InnerHeaderComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InnerHeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
