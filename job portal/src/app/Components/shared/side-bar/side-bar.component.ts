cbe364066a5d6fd3f7852817a1184a149b82ae7b
apurva-tambe <81083468+apurva-tambe@users.noreply.github.com>
2021-11-23 01:37:06 -0800

    fetching main branch

--- job portal/src/app/Components/shared/side-bar/side-bar.component.ts
+++ job portal/src/app/Components/shared/side-bar/side-bar.component.ts
@@ -1,24 +1,69 @@
-
-import { Component, OnInit } from '@angular/core';
-import { SigninService } from 'src/app/signin.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  selector: "app-side-bar",
+  templateUrl: "./side-bar.component.html",
+  styleUrls: ["./side-bar.component.scss"],
 })
-export class SidebarComponent implements OnInit {
-  
+export class SideBarComponent implements OnInit {
+  options = { autoHide: false, scrollbarMinSize: 100 };
+  menu: any = [
+    {
+      name: "Home",
+      url: "/dashboard",
+      hasChildren: false,
+      isActive: true,
+      subLinks: [],
+      icon: "fa fa-home",
+    },
 
-
-  
-  constructor(private  signinService: SigninService) { }
+    {
+      name: "My profile",
+      url: "/profile/about-you",
+      hasChildren: false,
+      isActive: false,
+      subLinks: [],
+      icon: "fa fa-home",
+    },
+    {
+      name: "Jobs",
+      url: "/jobs",
+      hasChildren: false,
+      isActive: false,
+      subLinks: [],
+      icon: "fa fa-home",
+    },
+    {
+      name: "Candidate pofile",
+      url: "/candidate-profile",
+      hasChildren: false,
+      isActive: true,
+      subLinks: [],
+      icon: "fa fa-home",
+    },
+    {
+      name: "Pofile summary",
+      url: "/profile-summary",
+      hasChildren: false,
+      isActive: true,
+      subLinks: [],
+      icon: "fa fa-home",
+    }
+  ];
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-
-  }
-  logout(){
-    this.signinService.logout()
+    this. makeLinkActive();
   }
 
+  makeLinkActive() {
+    this.menu.forEach((item: any) => {
+      if (item.url === this.router.url) {
+        item.isActive = true;
+      } else {
+        item.isActive = false;
+      }
+    });
+  }
 }
