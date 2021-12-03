81e2ec7d48a26567d98182cfc3c0d0983c3570bf
sandhya838 <78634804+sandhya838@users.noreply.github.com>
2021-12-03 10:06:09 -0500

    roleprofile changes

--- job portal/src/app/constants.ts
+++ job portal/src/app/constants.ts
@@ -135,6 +135,7 @@ export const CONSTANTS = {
   SECRET:
     "SiJIVnYKsw785iHCAZw6QY9hzi573lQnGlToW7LTWaEgtuV00DJFnUSxgMyrXoCgwk0ya1u2PYVRctsYGpJu9oDb1Jitb6gMYOkJujkHK2UNbYv9p8fdcY6dzzfYmRYgiu3mazCwaJXrhqJ7RCVjY7eddfNUFish",
   BASEURL: "http://localhost:3000/",
+  // BASEURL: "https://jobproofile-api.herokuapp.com/",
   RECOMMENDEDJOBS: "v1/recomandedJobs",
   USERPROFILE:BASEURL + "v1/profile/",
   CANDIDATESIGNUP: BASEURL + "v1/register",
